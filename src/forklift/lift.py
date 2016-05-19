b8f78b42cbfe61c1eee49abf028e842fdbecd22a
steveoh <sgourley@utah.gov>
2016-05-19 09:33:47 -0600

    implement init with tests
    :warning: not verified

---
+++ src/forklift/lift.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+lift.py
+
+A module that contains the implementation of the cli commands
+'''
+
+import logging
+import settings
+from json import dumps
+from os.path import abspath, exists
+
+log = logging.getLogger(settings.LOGGER)
+
+
+def init():
+    if exists('config.json'):
+        return 'forklift - config file already created.'
+
+    default_plugin_locations = ['c:\\scheduled']
+
+    log.debug('creating config.json file.')
+    with open('config.json', 'w') as json_data_file:
+        data = dumps(default_plugin_locations)
+
+        log.debug('writing %s to %s', data, abspath(json_data_file))
+        json_data_file.write(data)
+
+        return abspath(json_data_file)

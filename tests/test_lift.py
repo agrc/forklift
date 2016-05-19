2041eff6d55c10ce36c9651055e9571a63563137
steveoh <sgourley@utah.gov>
2016-05-19 11:41:22 -0600

    manage config.json in test

--- tests/test_lift.py
+++ tests/test_lift.py
@@ -9,13 +9,19 @@ A module that contains tests for the lift.py module
 import unittest
 from forklift import lift
 from json import loads
+from os import remove
 from os.path import exists
 
 
 class TestLift(unittest.TestCase):
 
     def setUp(self):
-        pass
+        if exists('config.json'):
+            remove('config.json')
+
+    def tearDown(self):
+        if exists('config.json'):
+            remove('config.json')
 
     def test_init_creates_config_file(self):
         path = lift.init()
@@ -23,4 +29,4 @@ class TestLift(unittest.TestCase):
         self.assertTrue(exists(path))
 
         with open(path) as config:
-            self.assertEquals(['c:\\scheduled'], loads(config))
+            self.assertEquals(['c:\\scheduled'], loads(config.read()))

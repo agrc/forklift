f0aaef2aa5af1113c30c828f06aa7a60c3e0a5c5
steveoh <sgourley@utah.gov>
2016-05-24 16:13:25 -0600

    add pallet tests

--- tests/test_pallet.py
+++ tests/test_pallet.py
@@ -7,7 +7,7 @@ A module that contains tests for the pallet module.
 '''
 
 import unittest
-from forklift.models import Pallet
+from forklift.models import Pallet, Crate
 
 
 class Pallet(Pallet):
@@ -75,3 +75,130 @@ class TestPallet(unittest.TestCase):
         self.assertEquals(self.patient.crates[1].source, 'source2')
         self.assertEquals(self.patient.crates[1].destination, 'destination2')
         self.assertEquals(self.patient.crates[1].destination_name, 'fc2_new')
+
+    def test_is_ready_to_ship_no_crates_returns_true(self):
+        self.assertTrue(self.patient.is_ready_to_ship())
+
+    def test_is_ready_to_ship_crates_with_updates_returns_true(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        self.patient._crates = [updated, updated, updated]
+
+        self.assertTrue(self.patient.is_ready_to_ship())
+
+    def test_is_ready_to_ship_crates_with_no_changes_returns_true(self):
+        no_changes = Crate(None, None, None, None)
+        no_changes.result = Crate.NO_CHANGES
+
+        self.patient._crates = [no_changes, no_changes, no_changes]
+
+        self.assertTrue(self.patient.is_ready_to_ship())
+
+    def test_is_ready_to_ship_crates_with_updates_and_no_changes_returns_true(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        no_changes = Crate(None, None, None, None)
+        no_changes.result = Crate.NO_CHANGES
+
+        self.patient._crates = [no_changes, updated, no_changes]
+
+        self.assertTrue(self.patient.is_ready_to_ship())
+
+    def test_is_ready_to_ship_crates_with_any_schema_changed_returns_false(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        no_changes = Crate(None, None, None, None)
+        no_changes.result = Crate.NO_CHANGES
+
+        schema_change = Crate(None, None, None, None)
+        schema_change.result = Crate.SCHEMA_CHANGED
+
+        self.patient._crates = [updated, no_changes, schema_change]
+
+        self.assertFalse(self.patient.is_ready_to_ship())
+
+    def test_is_ready_to_ship_crates_with_any_exception_returns_false(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        no_changes = Crate(None, None, None, None)
+        no_changes.result = Crate.NO_CHANGES
+
+        unhandled_exception = Crate(None, None, None, None)
+        unhandled_exception.result = Crate.UNHANDLED_EXCEPTION
+
+        self.patient._crates = [updated, no_changes, unhandled_exception]
+
+        self.assertFalse(self.patient.is_ready_to_ship())
+
+    def test_is_ready_to_ship_crates_with_all_returns_false(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        no_changes = Crate(None, None, None, None)
+        no_changes.result = Crate.NO_CHANGES
+
+        schema_change = Crate(None, None, None, None)
+        schema_change.result = Crate.SCHEMA_CHANGED
+
+        unhandled_exception = Crate(None, None, None, None)
+        unhandled_exception.result = Crate.UNHANDLED_EXCEPTION
+
+        self.patient._crates = [updated, no_changes, unhandled_exception, schema_change]
+
+        self.assertFalse(self.patient.is_ready_to_ship())
+
+    def test_requires_processing_with_no_crates_returns_false(self):
+        self.assertFalse(self.patient.requires_processing())
+
+    def test_requires_processing_crates_with_updates_returns_true(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        self.patient._crates = [updated, updated]
+
+        self.assertTrue(self.patient.requires_processing())
+
+    def test_requires_processing_crates_with_updates_and_changes_returns_true(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        no_changes = Crate(None, None, None, None)
+        no_changes.result = Crate.NO_CHANGES
+
+        self.patient._crates = [updated, no_changes, no_changes]
+
+        self.assertTrue(self.patient.requires_processing())
+
+    def test_requires_processing_crates_with_update_and_no_changes_returns_true(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        self.patient._crates = [updated, updated, updated]
+
+        self.assertTrue(self.patient.requires_processing())
+
+    def test_requires_processing_crates_with_schema_changes_returns_false(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        schema_change = Crate(None, None, None, None)
+        schema_change.result = Crate.SCHEMA_CHANGED
+
+        self.patient._crates = [schema_change, updated, updated]
+
+        self.assertFalse(self.patient.requires_processing())
+
+    def test_requires_processing_crates_with_unhandled_exception_returns_false(self):
+        updated = Crate(None, None, None, None)
+        updated.result = Crate.UPDATED
+
+        unhandled_exception = Crate(None, None, None, None)
+        unhandled_exception.result = Crate.UNHANDLED_EXCEPTION
+
+        self.patient._crates = [updated, updated, unhandled_exception]
+
+        self.assertFalse(self.patient.requires_processing())

1b3a658404e522707721fb108347bbf468f7ca94
Scott Davis <stdavis@utah.gov>
2016-05-24 09:37:29 -0600

    stub out test_forklift.py

---
+++ tests/test_forklift.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+test_forklift.py
+
+A module for testing forklift.py
+'''
+
+import unittest
+from forklift import forklift
+from forklift.models import Pallet, Crate

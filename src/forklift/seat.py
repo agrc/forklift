3bc61e48aa69832acc0006158d3ddd049ec3561f
Scott Davis <stdavis@utah.gov>
2016-06-01 08:27:59 -0600

    tests and fixes for format_time

--- src/forklift/seat.py
+++ src/forklift/seat.py
@@ -8,15 +8,17 @@ A module that contains helpful methods for other modules
 
 
 def format_time(seconds):
-    second = 1
-    minute = 60 * second
-    hour = 60 * minute
+    minute = 60.00
+    hour = 60.00 * minute
 
-    if seconds < 1 * minute:
-        return '{}ms'.format(round(seconds * 1000, 2))
+    if seconds < 30:
+        return '{} ms'.format(int(round(seconds * 1000, 0)))
 
-    if seconds < 90 * minute:
+    elif seconds < 90:
+        return '{} seconds'.format(seconds)
+
+    elif seconds < 90 * minute:
         return '{} minutes'.format(round(seconds / minute, 2))
 
-    if seconds < 24 * hour:
-        return '{} hours'.format(round(second / minute / hour, 2))
+    else:
+        return '{} hours'.format(round(seconds / hour, 2))

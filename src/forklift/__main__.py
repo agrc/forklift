afd9fd394552181bc815e99dc51228f5a0bbcd0d
steveoh <sgourley@utah.gov>
2016-06-02 16:12:09 -0600

    reorganize cli remove unnecessary --

--- src/forklift/__main__.py
+++ src/forklift/__main__.py
@@ -4,10 +4,11 @@
 forklift 🚜
 
 Usage:
-    forklift config --init
-    forklift config --add <folder-path>
-    forklift config --remove <folder-path>
-    forklift config --list
+    forklift config init
+    forklift config set --key <key> --value <value>
+    forklift config folder --add <folder-path>
+    forklift config folder --remove <folder-path>
+    forklift config folder list
     forklift list-pallets [<folder-path>]
     forklift lift [<file-path>]
 
@@ -16,14 +17,15 @@ Arguments:
     file-path       a path to a file
 
 Examples:
-    config --init                  creates the config file.
-    config --add path/to/folder    adds a path to the config. Checks for duplicates.
-    config --remove path/to/folder removes a path from the config.
-    config --list                  outputs the list of pallet folder paths in your config file.
-    list-pallets                   outputs the list of pallets from the config.
-    list-pallets path/to/folder    outputs the list of pallets for the passed in path.
-    lift                           the main entry for running all of pallets found in the config paths.
-    lift path/to/file              run a specific pallet.
+    python -m forklift config init                            creates the config file.
+    python -m forklift config set --key <key> --value <value> sets a key in the config with a value
+    python -m forklift config folder --add path/to/folder     adds a path to the config. Checks for duplicates.
+    python -m forklift config folder --remove path/to/folder  removes a path from the config.
+    python -m forklift config folder list                     outputs the list of pallet folder paths in your config file.
+    python -m forklift list-pallets                           outputs the list of pallets from the config.
+    python -m forklift list-pallets path/to/folder            outputs the list of pallets for the passed in path.
+    python -m forklift lift                                   the main entry for running all of pallets found in the config paths.
+    python -m forklift lift path/to/file                      run a specific pallets.
 '''
 
 import cli
@@ -32,24 +34,33 @@ import sys
 from docopt import docopt
 
 
+detailed_formatter = logging.Formatter(fmt='%(levelname).4s %(asctime)s %(module)s:%(lineno)-4s %(message)s', datefmt='%m-%d %H:%M:%S')
+cli.init()
+
+
 def main():
     args = docopt(__doc__, version='1.0.0')
     _setup_logging()
 
     if args['config']:
-        if args['--init']:
+        if args['init']:
             message = cli.init()
             print('config file: {}'.format(message))
 
-        if args['--add'] and args['<folder-path>']:
-            message = cli.add_config_folder(args['<folder-path>'])
+        if args['folder'] and args['<folder-path>']:
+            if args['--add']:
+                message = cli.add_config_folder(args['<folder-path>'])
+
+            if args['--remove']:
+                message = cli.remove_config_folder(args['<folder-path>'])
+
             print(message)
 
-        if args['--remove'] and args['<folder-path>']:
-            message = cli.remove_config_folder(args['<folder-path>'])
+        if args['set'] and args['<key>'] and args['<value>']:
+            message = cli.set_config_prop(args['<key>'], args['<value>'])
             print(message)
 
-        if args['--list']:
+        if args['folder'] and args['list']:
             for folder in cli.list_config_folders():
                 print(folder)
     elif args['list-pallets']:

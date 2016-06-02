c33e65253c5cd826affb5006e8b5ec905bf5771a
steveoh <sgourley@utah.gov>
2016-06-02 09:05:43 -0600

    add more sample pallets

--- samples/PalletSamples.py
+++ samples/PalletSamples.py
@@ -3,7 +3,8 @@
 '''
 PalletSamples.py
 
-A module that contains a simple Pallet to test forklift
+A module that contains a sample Pallets to test forklift. These can be thought of as acceptance tests.
+We should be able to run them twice without errors. Once to create, and once to check for updates.
 '''
 
 from arcpy import env
@@ -18,8 +19,8 @@ class StringCratePallet(Pallet):
         destination_workspace = env.scratchGDB
         source_workspace = 'Database Connections\\agrc@sgid10.sde'
 
-        self.add_crates(['Counties'], {'source_workspace': source_workspace,
-                                       'destination_workspace': destination_workspace})
+        self.add_crate('Counties', {'source_workspace': source_workspace,
+                                    'destination_workspace': destination_workspace})
 
 
 class ExplicitCratePallet(Pallet):
@@ -32,3 +33,40 @@ class ExplicitCratePallet(Pallet):
 
         crate_info = ('SGID10.GEOSCIENCE.AvalanchePaths', source_workspace, destination_workspace, 'AvyPaths')
         self.add_crate(crate_info)
+
+
+class OneValueTupleCratePallet(Pallet):
+
+    def __init__(self):
+        super(OneValueTupleCratePallet, self).__init__()
+
+        destination_workspace = env.scratchGDB
+        source_workspace = 'Database Connections\\agrc@sgid10.sde'
+
+        crate_info = ('SGID10.GEOSCIENCE.AvalanchePaths')
+        self.add_crate(crate_info, {'source_workspace': source_workspace,
+                                    'destination_workspace': destination_workspace})
+
+
+class ShapefileCratePallet(Pallet):
+
+    def __init__(self):
+        super(ShapefileCratePallet, self).__init__()
+
+        destination_workspace = env.scratchGDB
+        source_workspace = env.scratchFolder + '\\myshape.shp'
+
+        self.add_crate('myshape', {'source_workspace': source_workspace,
+                                   'destination_workspace': destination_workspace})
+
+
+class SdeCratePallet(Pallet):
+
+    def __init__(self):
+        super(SdeCratePallet, self).__init__()
+
+        destination_workspace = 'Database Connections\\UPDATE_TESTS.sde'
+        source_workspace = 'Database Connections\\agrc@sgid10.sde'
+
+        self.add_crate('SGID10.Boundaries.Counties', {'source_workspace': source_workspace,
+                                                      'destination_workspace': destination_workspace})

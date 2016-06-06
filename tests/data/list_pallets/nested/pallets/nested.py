bc49658a0053ec6b0d7d1173a9c3dcf11b3ed355
Scott Davis <stdavis@utah.gov>
2016-06-06 12:32:39 -0600

    recursively search for pallets
    Closes #33

---
+++ tests/data/list_pallets/nested/pallets/nested.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+nested.py
+
+A module that contains pallets to be used in test_lift.py tests
+'''
+
+from forklift.models import Pallet
+
+
+class NestedPallet(Pallet):
+
+    def __init__(self):
+        super(NestedPallet, self).__init__()

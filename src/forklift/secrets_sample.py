d028b9566334f7b1cee5421da6e46a2865a7f13c
Scott Davis <stdavis@utah.gov>
2016-06-06 09:27:34 -0600

    fix linting

--- src/forklift/secrets_sample.py
+++ src/forklift/secrets_sample.py
@@ -6,6 +6,6 @@ secrets_sample.py
 A skeleton implementation of secrets.py defining the properties to be filled out before successfully running the tool
 '''
 
-smtp_server = <server>
-smtp_port = <port number>
-from_address = <email address>
+smtp_server = '<server>'
+smtp_port = '<port number>'
+from_address = '<email address>'

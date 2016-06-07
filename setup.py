9cda11a558072d972e15441c61d7b2a3fc477ecb
steveoh <sgourley@utah.gov>
2016-06-07 15:53:51 -0600

    add colorama because :art:

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
     install_requires=[
         'docopt==0.6.2',
         'pystache==0.5.4',
+        'colorama==0.3.7',
         'gitpython==2.0.4',
         'requests==2.10.0',
         #: pyopenssl, ndg-httpsclient, pyasn1 are there to disable ssl warnings in requests

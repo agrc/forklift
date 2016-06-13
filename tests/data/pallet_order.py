d5158620575fc44bf6ef0f0914a41e50f5dad94c
Scott Davis <stdavis@utah.gov>
2016-06-13 08:28:46 -0600

    test for pallet order
    Closes #58

---
+++ tests/data/pallet_order.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+pallet_order.py
+
+A module that contains tests for pallet execution order
+'''
+
+
+from forklift.models import Pallet
+
+
+class PalletA(Pallet):
+    pass
+
+
+class PalletC(Pallet):
+    pass
+
+
+class PalletB(Pallet):
+    pass

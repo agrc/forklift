8a83b1d30941b2668cb1158cb08e245d4b3263e4
Scott Davis <stdavis@utah.gov>
2016-07-07 16:52:37 -0600

    Release v2.0.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class Tox(TestCommand):
 
 setup(
     name="forklift",
-    version="1.4.1",
+    version="2.0.0",
     license="MIT",
     description="CLI tool for managing automated tasks.",
     long_description="",
--- src/forklift/__main__.py
+++ src/forklift/__main__.py
@@ -52,7 +52,7 @@ detailed_formatter = logging.Formatter(fmt='%(levelname)-7s %(asctime)s %(module
 
 
 def main():
-    args = docopt(__doc__, version='1.4.1')
+    args = docopt(__doc__, version='2.0.0')
     _setup_logging(args['--verbose'])
     _add_global_error_handler()
 

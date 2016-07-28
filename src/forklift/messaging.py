19f5646bc023e41e8787a84639e24dedd007c73b
steveoh <sgourley@utah.gov>
2016-07-28 05:34:11 -0600

    send log file as attachment
    gmail clips the log forcing you to login to the server to look a it.
    Send it as an attachment. This commit also does not create the SMTP
    server unless we are sending emails. This removes the round trip to the
    server to authenticate. I've also added a spot to create local emails as
    emls for testing.

--- src/forklift/messaging.py
+++ src/forklift/messaging.py
@@ -9,15 +9,20 @@ A module that contains a method for sending emails
 import logging
 import secrets
 from config import get_config_prop
+from email import encoders
+from email import generator
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from os.path import isfile
+from os.path import join
 from smtplib import SMTP
 
 
 log = logging.getLogger('forklift')
 
 
-def send_email(to, subject, body):
+def send_email(to, subject, body, attachment, save_to_file=False):
     '''
     to: string | string[]
     subject: string
@@ -25,7 +30,6 @@ def send_email(to, subject, body):
 
     Send an email.
     '''
-
     if not isinstance(to, basestring):
         to_addresses = ','.join(to)
     else:
@@ -33,7 +37,7 @@ def send_email(to, subject, body):
 
     if isinstance(body, basestring):
         message = MIMEMultipart()
-        message.attach(MIMEText(body))
+        message.attach(MIMEText(body, 'html'))
     else:
         message = body
 
@@ -41,11 +45,26 @@ def send_email(to, subject, body):
     message['From'] = secrets.from_address
     message['To'] = to_addresses
 
-    smtp = SMTP(secrets.smtp_server, secrets.smtp_port)
-    if get_config_prop('sendEmails'):
+    if isfile(attachment):
+        log_file_attachment = MIMEBase('application', 'octet-stream')
+        log_file_attachment.add_header('Content-Disposition', 'attachment; filename="forklift.log"')
+
+        with(open(attachment, 'rb')) as log_file:
+            log_file_attachment.set_payload(log_file.read())
+
+        encoders.encode_base64(log_file_attachment)
+        message.attach(log_file_attachment)
+
+    if save_to_file:
+        from uuid import uuid4
+        with open(join('C:\\Projects\\TestData\\EmailPickup', str(uuid4()) + '.eml'), 'w') as outfile:
+            gen = generator.Generator(outfile)
+            gen.flatten(message)
+    elif get_config_prop('sendEmails'):
+        smtp = SMTP(secrets.smtp_server, secrets.smtp_port)
         smtp.sendmail(secrets.from_address, to, message.as_string())
         smtp.quit()
+
+        return smtp
     else:
         log.info('sendEmails is False. No email sent.')
-
-    return smtp

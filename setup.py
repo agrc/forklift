4379cbbde7c4f90c3b39fc43a08e73c6aeb2d9ca
steveoh <sgourley@utah.gov>
2017-02-07 20:03:24 -0700

    Release v5.0.2

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class Tox(TestCommand):
 
 setup(
     name="forklift",
-    version="5.0.1",
+    version="5.0.2",
     license="MIT",
     description="CLI tool for managing automated tasks.",
     long_description="",

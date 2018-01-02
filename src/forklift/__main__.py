fbe69fe9c954aba5c7e4cc413cf5fa011ea8c520
Scott Davis <stdavis@utah.gov>
2018-01-02 12:24:14 -0700

    remove extra return

--- src/forklift/__main__.py
+++ src/forklift/__main__.py
@@ -67,8 +67,6 @@ def main():
         log.error('activate the python 3 proenv to use forklift')
         return
 
-        return
-
     args = docopt(__doc__, version='8.4.1')
     _setup_logging(args['--verbose'])
     _add_global_error_handler()

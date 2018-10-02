1e6a1c0ca2f5b1ec675b3a488fd00cc3ede2a65c
steveoh <sgourley@utah.gov>
2018-10-02 17:21:53 -0600

    docs: add docstrings

--- src/forklift/arcgis.py
+++ src/forklift/arcgis.py
@@ -47,11 +47,14 @@ class LightSwitch(object):
         self.services_url = '{}/services'.format(base_url)
 
     def ensure(self, what):
-        '''ensures that affected_services are started or stopped with 5 attempts.
+        '''
         what: string 'stop' or 'start'
         server: dictionary of server to stop
 
-        returns the services that still did not do what was requested'''
+        ensures that affected_services are started or stopped with 5 attempts
+
+        returns the services that still did not do what was requested
+        '''
         tries = 4
         wait = [12, 8, 4, 2, 0]
         status = False
@@ -66,65 +69,12 @@ class LightSwitch(object):
 
         return status, message
 
-    def _execute(self, url):
-        self._check_token_freshness()
-
-        ok = (False, None)
-        data = {'f': 'json', 'token': self.token}
-
-        try:
-            r = requests.post(url, data=data, timeout=self.timeout)
-            r.raise_for_status()
-
-            ok = self._return_false_for_status(r.json())
-        except requests.exceptions.ConnectTimeout as t:
-            return (False, t)
-        except requests.exceptions.Timeout as t:
-            return (False, t)
-        except requests.exceptions.HTTPError as t:
-            return (False, t)
-
-        return ok
-
-    def _fetch(self, url):
-        self._check_token_freshness()
-
-        data = {'f': 'json', 'token': self.token}
-
-        r = requests.post(url, data=data, timeout=self.timeout)
-        r.raise_for_status()
-
-        return r.json()
-
-    def _check_token_freshness(self):
-        # check to make sure that token isn't expired
-        if self.token_expire_milliseconds <= time() * 1000:
-            self._request_token()
-
-    def _return_false_for_status(self, json_response):
-        if 'status' in list(json_response.keys()) and json_response['status'] == 'error':
-            if 'Token Expired.' in json_response['messages']:
-                self._request_token()
-
-                return (True, 'Requested new token')
-            else:
-                return (False, '; '.join(json_response['messages']))
-
-        return (True, None)
-
-    def _request_token(self):
-        data = {'username': self.username, 'password': self.password, 'client': 'requestip', 'expiration': 60, 'f': 'json'}
-
-        response = requests.post(self.token_url, data=data)
-        response.raise_for_status()
-
-        response_data = response.json()
-        self._return_false_for_status(response_data)
-
-        self.token = response_data['token']
-        self.token_expire_milliseconds = int(response_data['expires'])
 
     def validate_service_state(self):
+        '''Validates that services that are configured to be started are actually started
+
+        Returns a list of services that did not start properly
+        '''
         if not self._started:
             return []
 
@@ -148,3 +98,85 @@ class LightSwitch(object):
                 services.setdefault(self.server_qualified_name, []).append(service_path)
 
         return services
+
+
+    def _execute(self, url):
+        '''url: string
+
+        Posts to `url` ensuring a succussful status after getting a token from server.
+        Does not return any response data from the server.
+
+        Retuns a tuple with a boolean status and a message
+        '''
+        self._check_token_freshness()
+
+        ok = (False, None)
+        data = {'f': 'json', 'token': self.token}
+
+        try:
+            r = requests.post(url, data=data, timeout=self.timeout)
+            r.raise_for_status()
+
+            ok = self._return_false_for_status(r.json())
+        except requests.exceptions.ConnectTimeout as t:
+            return (False, t)
+        except requests.exceptions.Timeout as t:
+            return (False, t)
+        except requests.exceptions.HTTPError as t:
+            return (False, t)
+
+        return ok
+
+    def _fetch(self, url):
+        '''url: string
+
+        Posts to `url` ensuring a succussful status after getting a token from server.
+        Returns the response data from the server.
+
+        Retuns the json response from the server
+        '''
+        self._check_token_freshness()
+
+        data = {'f': 'json', 'token': self.token}
+
+        r = requests.post(url, data=data, timeout=self.timeout)
+        r.raise_for_status()
+
+        return r.json()
+
+    def _check_token_freshness(self):
+        '''checks the token expiration and requests a new one if it has expired
+        '''
+        if self.token_expire_milliseconds <= time() * 1000:
+            self._request_token()
+
+    def _return_false_for_status(self, json_response):
+        '''json_reponse: string - a json payload from a server
+
+        looks for a status in the json reponse and makes sure it does not contain an error
+
+        Returns a tuple with a boolean status and a message
+        '''
+        if 'status' in list(json_response.keys()) and json_response['status'] == 'error':
+            if 'Token Expired.' in json_response['messages']:
+                self._request_token()
+
+                return (True, 'Requested new token')
+            else:
+                return (False, '; '.join(json_response['messages']))
+
+        return (True, None)
+
+    def _request_token(self):
+        '''Makes a request to the token service and stores the token information
+        '''
+        data = {'username': self.username, 'password': self.password, 'client': 'requestip', 'expiration': 60, 'f': 'json'}
+
+        response = requests.post(self.token_url, data=data)
+        response.raise_for_status()
+
+        response_data = response.json()
+        self._return_false_for_status(response_data)
+
+        self.token = response_data['token']
+        self.token_expire_milliseconds = int(response_data['expires'])

f2c69e402c746d1e8a220ce6cb12e397bf45fc22
Scott Davis <stdavis@utah.gov>
2020-02-11 12:13:10 -0700

    fix: allow for UNC pallet paths

--- src/forklift/slack.py
+++ src/forklift/slack.py
@@ -90,7 +90,7 @@ def lift_report_to_blocks(report):
         if _safely_access(pallet, 'success'):
             success = ':heavy_check_mark:'
 
-        message.add(SectionBlock(f'{success} *{_safely_access(pallet, "name").split(":")[1]}*'))
+        message.add(SectionBlock(f'{success} *{_safely_access(pallet, "name").split(":")[-1]}*'))
         message.add(ContextBlock([f'{_safely_access(pallet, "total_processing_time")}{"  |  " + _safely_access(pallet, "message") if _safely_access(pallet, "message") else ""}']))
 
         crate_elements = []
@@ -189,7 +189,7 @@ def ship_report_to_blocks(report):
         if _safely_access(pallet, 'success'):
             success = ':heavy_check_mark:'
 
-        message.add(SectionBlock(f'{success} *{_safely_access(pallet, "name").split(":")[1]}*'))
+        message.add(SectionBlock(f'{success} *{_safely_access(pallet, "name").split(":")[-1]}*'))
 
         post_copy_processed = shipped = ':red_circle:'
         if _safely_access(pallet, 'post_copy_processed'):
